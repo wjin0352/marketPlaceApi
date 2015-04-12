29751c5464aa85f63d861f76182d05d7cf6275e8
won <wjin@nyit.edu>
2015-04-12 01:37:11 -0400

    Set the routes contraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,20 @@ MarketPlaceApi::Application.routes.draw do
 # we are just going to set the constraints,
 # the base_uri and the default response format for each request.
 
+# We are going to isolate the api controllers under a namespace,
+# in Rails this is fairly simple, just create a folder under the
+# app/controllers named api, the name is important as it is the namespace
+# we’ll use for managing the controllers for the api endpoints
+
+# Defining namespace under 'api', By defining a namespace under the routes.rb file. Rails will
+# automatically map that namespace to a directory matching the name under the
+# controllers folder, in our case the api/ directory.
+
+  namespace :api, defaults: { format: :json },
+                  constraints: { subdomain: 'api' }, path: '/' do
+
+  #   listing our resources here
+  end
+
 
 end
